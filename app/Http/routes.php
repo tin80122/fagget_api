4aa30684b6f7741037ccd1876ecd6c503c076c1f
sophie <z5678914@gmail.com>
2018-08-12 17:22:29 +0800

    routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,15 +18,19 @@ Route::get('/', function () {
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
+	$api->post('register_token','App\Http\Controllers\Auth\AuthenticateController@register');
+	$api->post('test_token','App\Http\Controllers\Auth\AuthenticateController@authenticate');
+	//$api->group(['middleware'=>'jwt.auth'],function ($api){
+		$api->post('register','App\Http\Controllers\Auth\AuthenticateController@register');
 
- $api->post('add_article', 'App\Http\Controllers\Auth\ArticleController@createArticle');
- $api->post('get_article', 'App\Http\Controllers\Auth\ArticleController@getArticle');
+		$api->post('add_article', 'App\Http\Controllers\Auth\ArticleController@createArticle');
+		$api->post('get_article', 'App\Http\Controllers\Auth\ArticleController@getArticle');
 
- $api->post('add_category', 'App\Http\Controllers\Auth\CategoryController@createCategory');
- $api->post('get_category', 'App\Http\Controllers\Auth\CategoryController@getCategory');
+		$api->post('add_category', 'App\Http\Controllers\Auth\CategoryController@createCategory');
+		$api->post('get_category', 'App\Http\Controllers\Auth\CategoryController@getCategory');
 
- $api->post('register_member', 'App\Http\Controllers\Auth\MemberController@registerMember');
- $api->post('login', 'App\Http\Controllers\Auth\MemberController@login');
+		$api->post('register_member', 'App\Http\Controllers\Auth\MemberController@registerMember');
+		$api->post('login', 'App\Http\Controllers\Auth\MemberController@login');
 
-
+	//});
 });
